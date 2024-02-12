aa266aed5e7867f303e318c0d5b4e0e5d8fcd62c
Arnold <akseong@uci.edu>
2024-02-11 21:41:24 -0800

    tutorial 1 completed

--- torch_tutorials/tut_1_guess_cor.R
+++ torch_tutorials/tut_1_guess_cor.R
@@ -135,4 +135,108 @@ length(test_dl)
 # # # # # # # # # # # # # # # # # # # # # # # # #
 ## Create the model ----
 # # # # # # # # # # # # # # # # # # # # # # # # #
+torch_manual_seed(777)
 
+net <- nn_module(
+  
+  "corr-cnn",
+  
+  # initialize any submodules (i.e. layers)
+  # initialize() defines the layers
+  initialize = function() {
+    
+    # 3 convolutional layers (filter/kernel)
+    # 32 3x3 filters in first layer, 64 3x3 filters in 2nd layer, 128 3x3 in 3rd
+    self$conv1 <- nn_conv2d(in_channels = 1, out_channels = 32, kernel_size = 3)
+    self$conv2 <- nn_conv2d(in_channels = 32, out_channels = 64, kernel_size = 3)
+    self$conv3 <- nn_conv2d(in_channels = 64, out_channels = 128, kernel_size = 3)
+    
+    # linear layers
+    self$fc1 <- nn_linear(in_features = 14 * 14 * 128, out_features = 128)
+    self$fc2 <- nn_linear(in_features = 128, out_features = 1)
+    
+  },
+  
+  # forward() specifies order in which to call layers
+  # and what to do "in between"
+  forward = function(x) {
+    
+    x %>% 
+      self$conv1() %>%          # invoke conv layer
+      nnf_relu() %>%            # relu activation fcn
+      nnf_avg_pool2d(2) %>%     # downsizes image --- replaces 
+                                # 2x2 patch of pixels by its average
+      # pooling layer: "while the number of channels increases
+      # (1 --> 32 --> 64 --> 128), we decrease spatial resolution"
+      
+      self$conv2() %>%
+      nnf_relu() %>%
+      nnf_avg_pool2d(2) %>%
+      
+      self$conv3() %>%
+      nnf_relu() %>%
+      nnf_avg_pool2d(2) %>%
+      
+      # torch_flatten reshapes inputs (no computation)
+      # goes from 4-dimensional structure outputted by 2nd conv layer
+      # to 2-dimensional one expected by first linear layer
+      torch_flatten(start_dim = 2) %>%
+      self$fc1() %>%
+      nnf_relu() %>%
+      
+      self$fc2()
+  }
+)
+
+model <- net()
+model(batch$x)
+
+#   # run model to obtain current preds
+#   output <- model(batch$x)
+#
+#   # calculate the loss
+#   loss <- nnf_mse_loss(output, batch$y$unsqueeze(2))
+#
+#   # backwards propagation to compute gradients
+#   loss$backward()
+#
+#   # update parameters
+#   optimizer$step()
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # #
+## USE luz TO FIT MODEL ----
+# # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+# setup() --- specify loss function (here, MSE) and optimization algorithm
+# fit() --- passes training data loader, number training epochs, and validation data loader
+#           validation set used to compute test loss only (does not contribute to param updates)
+fitted <- net %>%
+  setup(
+    loss = function(y_hat, y_true) nnf_mse_loss(y_hat, y_true$unsqueeze(2)),
+    optimizer = optim_adam
+  ) %>%
+  fit(train_dl, epochs = 10, valid_data = test_dl)
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # #
+## EVALUATE PERFORMANCE ----
+# # # # # # # # # # # # # # # # # # # # # # # # #
+
+preds <- predict(fitted, test_dl)
+?predict
+
+preds <- preds$to(device = "cpu")$squeeze() %>% as.numeric()
+test_dl <- dataloader(test_ds, batch_size = 5000)
+targets <- (test_dl %>% dataloader_make_iter() %>% dataloader_next())$y %>% as.numeric()
+
+df <- data.frame(preds = preds, targets = targets)
+
+library(ggplot2)
+
+ggplot(df, aes(x = targets, y = preds)) +
+  geom_point(size = 0.1) +
+  theme_classic() +
+  xlab("true correlations") +
+  ylab("model predictions")
