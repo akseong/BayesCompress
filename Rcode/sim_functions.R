ef3361b8e471feab46524877c7eba129d7b0f8cd
akseong <akseong@uci.edu>
2025-08-09 13:45:36 -0700

    added `sim_fcn_hshoe_linreg()` (simulation function for hshoe model in basic linear regression setting)

--- Rcode/sim_functions.R
+++ Rcode/sim_functions.R
@@ -8,11 +8,11 @@
 ## sim_linear_data ----
 sim_linear_data <- function(
   n_obs = 100,
+  err_sigma = 1,
+  true_coefs = NULL,
   d_in = 10,
   d_true = 3,
-  err_sigma = 1,
-  intercept = 0,
-  true_coefs = NULL
+  intercept = 0
 ){
   require(torch)
   
@@ -367,5 +367,317 @@ get_lm_stats <- function(simdat, alpha = 0.05){
 
 
 # FOR LASSO ----
+
+
+
+# SIMULATION FCNS ----
+## horseshoe, basic linear regression setting ----
+sim_fcn_hshoe_linreg <- function(
+    sim_ind,    # to ease parallelization
+    sim_params,     # see example
+    nn_model,   # torch nn_module,
+    train_epochs = 10000,
+    verbose = TRUE,      # provide updates in console
+    report_every = 1000, # training epochs between display/store results
+    want_plots = TRUE,   # provide graphical updates of KL, MSE
+    want_all_params = FALSE, 
+    want_data = FALSE    # include training data in output
+){
+  # - fcn used with lapply for parallel processing
+  # - basic linear regression setting
+  # - only stop criteria used is max epochs
+  # - if want_all_params = FALSE, only returns
+  #     loss_mat (KL, mse vs epoch) and
+  #     alpha_mat (local dropout rates vs epoch)
+  # USAGE EXAMPLE:
+  # > sim_params <- list(
+  # >   "sim_name" = "horseshoe, linear regression setting, KL scaled by n",
+  # >   "n_sims" = 100,
+  # >   "d_in" = 104,
+  # >   "n_obs" = 125,
+  # >   "true_coefs" = c(-0.5, 1, -2, 4, rep(0, times = 100)),
+  # >   "seed" = 314,
+  # >   "err_sig" = 1,
+  # >   "ttsplit" = 4/5,
+  # >   "stop_criteria" = c("test_train","ma_loss_increasing")
+  # > )
+  # >
+  # > res <- lapply(
+  # >   1:sim_params$n_sims, 
+  # >   function(X) sim_fcn_hshoe_linreg(
+  # >     sim_ind = X, 
+  # >     sim_params = sim_params,
+  # >     nn_model = SLHS,  #### this needs to be pre-defined via torch::nn_module()
+  # >     train_epochs = 1000,
+  # >     verbose = TRUE,
+  # >     report_every = 100,
+  # >     want_plots = TRUE,
+  # >     want_all_params = FALSE,
+  # >     want_data = FALSE
+  # >   )
+  # > )
+  
+  ## generate linear data
+  set.seed(sim_params$sim_seeds[sim_ind])
+  torch_manual_seed(sim_params$sim_seeds[sim_ind])
+  
+  lin_simdat <- sim_linear_data(
+    n = sim_params$n_obs,
+    true_coefs = sim_params$true_coefs,
+    err_sigma = sim_params$err_sig
+  )
+  
+  # store: # train, test mse and kl
+  report_epochs <- seq(
+    report_every, 
+    train_epochs, 
+    by = report_every
+  )
+  
+  loss_mat <- matrix(
+    NA, 
+    nrow = length(report_epochs),
+    ncol = 3
+  )
+  colnames(loss_mat) <- c("kl", "mse_train", "mse_test")
+  rownames(loss_mat) <- report_epochs
+  
+  
+  # store: alphas
+  alpha_mat <- matrix(
+    NA, 
+    nrow = length(report_epochs),
+    ncol = sim_params$d_in
+  )
+  rownames(alpha_mat) <- report_epochs
+  
+  # store: weight posterior params
+  if (want_all_params){
+    w_mu_mat <-
+      w_var_mat <- 
+      marginal_dropout_mat <-
+      atilde_mu_mat <- 
+      btilde_mu_mat <-
+      atilde_logvar_mat <-
+      btilde_logvar_mat <- alpha_mat
+    
+    global_dropout_vec <-   
+      tau_vec <- 
+      sa_mu_vec <-
+      sb_mu_vec <- 
+      sa_logvar_vec <- 
+      sb_logvar_vec <- rep(NA, length(report_epochs))
+  }
+  
+  
+  ## SETUP STOP CRITERIA ----
+  #### 
+  ####  NOT FULLY IMPLEMENTED IN THIS FILE.  ONLY FOR STORAGE / VIEWING. 
+  ####  ONLY STOPPING CRITERIA HERE IS MAX_EPOCHS.
+  ####
+  ## test-train split
+  
+  ttsplit_used <- "test_train" %in% sim_params$stop_criteria || "test_convergence" %in% sim_params$stop_criteria
+  ttsplit_ind <- ifelse(
+    ttsplit_used,
+    floor(sim_params$n_obs * sim_params$ttsplit),
+    sim_params$n_obs
+  )
+  
+  if (ttsplit_used){
+    x_test <- lin_simdat$x[(ttsplit_ind+1):sim_params$n_obs, ] 
+    y_test <- lin_simdat$y[(ttsplit_ind+1):sim_params$n_obs, ]
+    loss_test <- torch_zeros(1)  # set initial value
+    loss_diff_test <- 1          # set initial value    
+  }
+  
+  x_train <- lin_simdat$x[1:ttsplit_ind, ]
+  y_train <- lin_simdat$y[1:ttsplit_ind, ]
+  
+  ## initialize BNN & optimizer ----
+  slhs_net <- nn_model()
+  optim_slhs <- optim_adam(slhs_net$parameters)
+  
+  ## TRAINING LOOP ----
+  ## initialize training params
+  loss_diff <- 1
+  loss <- torch_zeros(1)
+  epoch <- 0
+  stop_criteria_met <- FALSE
+  
+  while (!stop_criteria_met){
+    prev_loss <- loss
+    epoch <- epoch + 1
+    stop_criteria_met <- epoch > train_epochs
+    
+    
+    # fit & metrics
+    yhat_train <- slhs_net(x_train)
+    mse <- nnf_mse_loss(yhat_train, y_train)
+    kl <- slhs_net$get_model_kld() / ttsplit_ind
+    loss <- mse + kl
+    # loss_diff <- loss - prev_loss
+    
+    
+    # gradient step 
+    # zero out previous gradients
+    optim_slhs$zero_grad()
+    # backprop
+    loss$backward()
+    # update weights
+    optim_slhs$step()
+    
+    
+    # compute test loss 
+    if (ttsplit_used) {
+      prev_loss_test <- loss_test
+      yhat_test <- slhs_net(x_test) 
+      # **WOULD LIKE THIS TO BE DETERMINISTIC, i.e. based on post pred means** ----
+      mse_test <- nnf_mse_loss(yhat_test, y_test)
+    }
+    
+    
+    # store results (every `report_every` epochs) ----
+    time_to_report <- epoch!=0 & (epoch %% report_every == 0)
+    if (time_to_report){
+      row_ind <- epoch %/% report_every
+      
+      loss_mat[row_ind, ] <- c(kl$item(), mse$item(), mse_test$item())
+      dropout_alphas <- slhs_net$fc1$get_dropout_rates()
+      alpha_mat[row_ind, ] <- as_array(dropout_alphas)
+      
+      # storing other optional parameters, mostly for diagnostics
+      if (want_all_params){
+        global_dropout_vec[row_ind] <- as_array(slhs_net$fc1$get_dropout_rates(type = "global"))
+        marginal_dropout_mat[row_ind, ] <- as_array(slhs_net$fc1$get_dropout_rates(type = "marginal"))
+        tau_vec[row_ind] <- as_array(slhs_net$fc1$tau)
+        w_mu_mat[row_ind, ] <- as_array(slhs_net$fc1$compute_posterior_param()$post_weight_mu)
+        w_var_mat[row_ind, ] <- as_array(slhs_net$fc1$compute_posterior_param()$post_weight_var)
+        
+        atilde_mu_mat[row_ind, ] <- as_array(slhs_net$fc1$atilde_mu)
+        btilde_mu_mat[row_ind, ] <- as_array(slhs_net$fc1$btilde_mu)
+        atilde_logvar_mat[row_ind, ] <- as_array(slhs_net$fc1$atilde_logvar)
+        btilde_logvar_mat[row_ind, ] <- as_array(slhs_net$fc1$btilde_logvar)
+        sa_mu_vec[row_ind] <- as_array(slhs_net$fc1$sa_mu)
+        sa_logvar_vec[row_ind] <- as_array(slhs_net$fc1$sa_logvar)
+        sb_mu_vec[row_ind] <- as_array(slhs_net$fc1$sb_mu)
+        sb_logvar_vec[row_ind] <- as_array(slhs_net$fc1$sb_logvar)
+      }
+    } # end result storing and updating
+    
+    
+    # in-console and graphical training updates ----
+    if (time_to_report & verbose){
+      cat(
+        "Epoch:", epoch,
+        "MSE + KL/n =", mse$item(), "+", kl$item(),
+        "=", loss$item(),
+        "\n",
+        "train mse:", round(mse$item(), 4), 
+        "; test_mse:", round(mse_test$item(), 4),
+        "\n", 
+        sep = " "
+      )
+      cat("alphas: ", round(as_array(dropout_alphas), 2), "\n")
+      display_alphas <- ifelse(
+        as_array(dropout_alphas) <= .1,
+        round(as_array(dropout_alphas), 3),
+        "."
+      )
+      cat("alphas below 0.1: ")
+      cat_color(display_alphas, sep = " ")
+      cat(" \n \n")
+      
+      
+      # graphical training updates
+      if (want_plots & row_ind > 5){
+        
+        # only show most recent (for scale of plot)
+        start_plot_row_ind <- 1
+        if (row_ind >= 20){
+          start_plot_row_ind <- row_ind - 10
+        } else if (row_ind >= 60){
+          start_plot_row_ind <- row_ind - 30
+        }
+        # set up plotting df
+        temp_lmat <- cbind(
+          loss_mat[start_plot_row_ind:row_ind,],
+          "epoch" = report_epochs[start_plot_row_ind:row_ind]
+        )
+        
+        mse_plotdf <- temp_lmat %>% 
+          data.frame() %>% 
+          pivot_longer(
+            cols = -epoch,
+            names_to = "metric"
+          )
+        
+        # plot MSE vs epochs
+        mse_plt <- mse_plotdf %>% 
+          filter(metric != "kl") %>% 
+          ggplot(aes(y = value, x = epoch, color = metric)) + 
+          geom_line() + 
+          labs(
+            subtitle = paste0("MSE for epochs ", 
+                              report_epochs[start_plot_row_ind],
+                              " through ",
+                              report_epochs[row_ind])
+          )
+        
+        # plot KL vs epochs
+        kl_plt <- mse_plotdf %>%
+          filter(metric == "kl") %>%
+          ggplot(aes(y = value, x = epoch, color = metric)) +
+          geom_line() +
+          labs(
+            subtitle = paste0("KL for epochs ",
+                              report_epochs[start_plot_row_ind],
+                              " through ",
+                              report_epochs[row_ind])
+          )
+        grid_plt <- gridExtra::grid.arrange(
+          grobs = list(mse_plt, kl_plt),
+          nrow = 2
+        )
+        print(grid_plt)
+      } # end graphical training updates (want_plots = TRUE)
+    } # end training updates (verbose = TRUE)
+    
+  } # end training WHILE loop
+  
+  # compile results ----
+  sim_res <- list(
+    "sim_ind" = sim_ind,
+    "loss_mat" = loss_mat,
+    "alpha_mat" = alpha_mat
+  )
+  
+  if (want_all_params){
+    # add optionally stored network params
+    sim_res$w_mu_mat <- w_mu_mat
+    sim_res$w_var_mat <- w_var_mat
+    sim_res$global_dropout_vec <- global_dropout_vec
+    sim_res$marginal_dropout_mat <- marginal_dropout_mat
+    sim_res$atilde_mu_mat <- atilde_mu_mat
+    sim_res$btilde_mu_mat <- btilde_mu_mat
+    sim_res$atilde_logvar_mat <- atilde_logvar_mat
+    sim_res$btilde_logvar_mat <- btilde_logvar_mat
+    sim_res$sa_mu_vec <- sa_mu_vec
+    sim_res$sb_mu_vec <- sb_mu_vec
+    sim_res$sa_logvar_vec <- sa_logvar_vec
+    sim_res$sb_logvar_vec <- sb_logvar_vec
+    sim_res$tau_vec <- tau_vec
+  } 
+  
+  if (want_data){
+    # add data
+    sim_res$x = as_array(lin_simdat$x)
+    sim_res$y = as_array(lin_simdat$y)
+    sim_res$true_coefs = lin_simdat$true_coefs
+  }
+  
+  return(sim_res)
+}
+
 
 
