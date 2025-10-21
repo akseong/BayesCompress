6101b616dbf54ce59b6d9a673f9a744671ceba36
Arnold <akseong@uci.edu>
2025-10-20 20:29:01 -0700

    init

---
+++ Rcode/testing_nn_archs.R
@@ -0,0 +1,36 @@
+##################################################
+## Project:   train networks of various sizes
+## Date:      Sep 24, 2025
+## Author:    Arnie Seong
+##################################################
+
+
+#### setup ----
+library(here)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(gridExtra)
+
+library(torch)
+source(here("Rcode", "torch_horseshoe.R"))
+source(here("Rcode", "sim_functions.R"))
+
+
+if (torch::cuda_is_available()){
+  use_cuda <- TRUE
+  message("Default tensor device set to GPU (CUDA).")
+} else {
+  use_cuda <- FALSE
+  message("Default tensor device remains CPU.")
+}
+
+
+
+
+
+
+
+
+
+
