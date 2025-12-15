d00aad8f2704a7deeb40db10b22f3e049d288849
Arnold <akseong@uci.edu>
2025-12-15 13:32:13 -0800

    minor edit: corrected simname

--- Rcode/smoother_fcns_kaiming.R
+++ Rcode/smoother_fcns_kaiming.R
@@ -71,7 +71,7 @@ save_mod_path_prestem <- here::here(
 )
 
 sim_params <- list(
-  "sim_name" = "tau_0 = .5, kaiming init, 2 layers 16 8, nobatching, fcnal data.  ",
+  "sim_name" = "tau_0 = 1, kaiming init, 2 layers 16 8, nobatching, fcnal data.  ",
   "seed" = 21683,
   "n_sims" = 1, 
   "train_epochs" = 15E5,
