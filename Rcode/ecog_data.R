0d491f48f095023820e64ff8f86934aab8802d77
akseong <akseong@uci.edu>
2026-02-17 09:56:29 -0800

    init

---
+++ Rcode/ecog_data.R
@@ -0,0 +1,238 @@
+##################################################
+## Project:   ecog data
+## Date:      Feb 3, 2026
+## Author:    Arnie Seong
+##################################################
+
+#### setup ----
+library(here)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(gridExtra)
+
+library(torch)
+source(here("Rcode", "torch_horseshoe_klcorrected.R"))
+source(here("Rcode", "sim_functions.R"))
+
+if (torch::cuda_is_available()){
+  use_cuda <- TRUE
+  message("Default tensor device set to GPU (CUDA).")
+} else {
+  use_cuda <- FALSE
+  message("Default tensor device remains CPU.")
+}
+
+save_mod_path_prestem <- here::here(
+  "sims", 
+  "results", 
+  "salary_sim_314"
+)
+
+sim_params <- list(
+  "sim_name" = "salary data 16-32-1",
+  "seed" = 314,
+  "n_sims" = 5, 
+  "train_epochs" = 5E5,
+  "report_every" = 1E4,
+  "use_cuda" = use_cuda,
+  "d_in" = 104,
+  "d_hidden1" = 8,
+  "d_hidden2" = 16,
+  # "d_hidden3" = 16,
+  # "d_hidden4" = 16,
+  # "d_hidden5" = 16,
+  "d_out" = 1,
+  "n_obs" = 1250,
+  "lr" = 0.001,  # sim_hshoe learning rate arg.  If not specified, uses optim_adam default (0.001)
+  "err_sig" = 1,
+  "xdist" = "norm"
+)
+set.seed(sim_params$seed)
+sim_params$sim_seeds <- floor(runif(n = sim_params$n_sims, 0, 1000000))
+
+
+
+
+# SALARY DATA ----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# calibrate tau
+# Piironen & Vehtari 2017 suggest tau_0 = p_0 / (d - p_0) * sig / sqrt(n)
+# where p_0 = prior estimate of number of nonzero betas, d = total number of covs
+
+sim_params$prior_tau <- tau0_PV(
+  p_0 = 10, d = 104, sig = 1, 
+  n = round(sim_params$n_obs * sim_params$ttsplit)
+)
+
+agnostic_tau <- tau0_PV(
+  p_0 = 1, d = 2, sig = 1, 
+  n = round(sim_params$n_obs * sim_params$ttsplit)
+)
+
+## define model
+MLHS <- nn_module(
+  "MLHS",
+  initialize = function() {
+    self$fc1 = torch_hs(    
+      in_features = sim_params$d_in, 
+      out_features = sim_params$d_hidden1,
+      use_cuda = sim_params$use_cuda,
+      tau_0 = sim_params$prior_tau,
+      init_weight = NULL,
+      init_bias = NULL,
+      init_alpha = 0.9,
+      clip_var = TRUE
+    )
+    
+    self$fc2 = torch_hs(
+      in_features = sim_params$d_hidden1,
+      out_features = sim_params$d_hidden2,
+      use_cuda = sim_params$use_cuda,
+      tau_0 = agnostic_tau,
+      init_weight = NULL,
+      init_bias = NULL,
+      init_alpha = 0.9,
+      clip_var = TRUE
+    )
+    
+    self$fc3 = torch_hs(
+      in_features = sim_params$d_hidden2,
+      #   out_features = sim_params$d_hidden3,
+      #   use_cuda = sim_params$use_cuda,
+      #   tau = agnostic_tau,
+      #   init_weight = NULL,
+      #   init_bias = NULL,
+      #   init_alpha = 0.9,
+      #   clip_var = TRUE
+      # )
+      # 
+      # self$fc4 = torch_hs(
+      #   in_features = sim_params$d_hidden3,
+      #   out_features = sim_params$d_hidden4,
+      #   use_cuda = sim_params$use_cuda,
+      #   tau = agnostic_tau,
+      #   init_weight = NULL,
+      #   init_bias = NULL,
+      #   init_alpha = 0.9,
+      #   clip_var = TRUE
+      # )
+      # 
+      # self$fc5 = torch_hs(
+      #   in_features = sim_params$d_hidden4,
+      out_features = sim_params$d_out,
+      use_cuda = sim_params$use_cuda,
+      tau_0 = agnostic_tau,
+      init_weight = NULL,
+      init_bias = NULL,
+      init_alpha = 0.9,
+      clip_var = TRUE
+    )
+    
+  },
+  
+  forward = function(x) {
+    x %>%
+      self$fc1() %>%
+      nnf_relu() %>%
+      self$fc2() %>%
+      nnf_relu() %>%
+      self$fc3() # %>%
+      # nnf_relu() %>%
+      # self$fc4() %>%
+      # nnf_relu() %>%
+      # self$fc5()
+  },
+  
+  
+  
+  get_model_kld = function(){
+    kl1 = self$fc1$get_kl()
+    kl2 = self$fc2$get_kl()
+    kl3 = self$fc3$get_kl()
+    # kl4 = self$fc3$get_kl()
+    # kl5 = self$fc3$get_kl()
+    kld = kl1 + kl2 + kl3 #+ kl4 + kl5
+    return(kld)
+  }
+)
+# 
+# sim_params$model <- MLHS
+# mod <- MLHS()
+# mod$fc1$parameters
+# ztil <- get_ztil_sq(mod$fc1)
+# s <- get_s_sq(mod$fc1)
+# Wz_params <- get_Wz_params(mod$fc1)
+# Wz_mu <- Wz_params$Wz_mu
+# W_mu <- as_array(mod$fc1$weight_mu)
+# mean(diag(cov(W_mu)))
+# 
+# 
+# nn_init_kaiming_normal_(mod$fc1$weight_mu)
+# W_mu_k <- as_array(mod$fc1$weight_mu)
+# sum(diag(cov(W_mu_k)))
+# sum(diag(cov(W_mu)))
+# mean(diag(cov(W_mu_k)))
+# mean(W_mu_k)
+# mean(W_mu)
+
+# verbose = TRUE
+# want_plots = TRUE
+# want_fcn_plots = TRUE
+# save_fcn_plots = FALSE
+# want_all_params = FALSE
+# save_mod = TRUE
+# save_mod_path_stem = NULL
+# nn_model <- MLHS
+
+res <- lapply(
+  1:sim_params$n_sims,
+  function(X) {
+    save_mod_path_stem <- paste0(
+      save_mod_path_prestem,
+      sim_params$n_obs, "obs_", 
+      sim_params$sim_seeds[X]
+    )
+    
+    sim_hshoe(
+      sim_ind = X,
+      sim_params = sim_params,     # same as before, but need to include flist
+      # learning_rate = sim_params$lr,
+      nn_model = MLHS,   # torch nn_module,
+      verbose = TRUE,      # provide updates in console
+      want_plots = FALSE,   # provide graphical updates of KL, MSE
+      want_fcn_plots = TRUE, # display predicted functions
+      save_fcn_plots = TRUE,
+      want_all_params = TRUE,
+      save_mod = TRUE,
+      save_mod_path_stem = save_mod_path_stem
+    )
+  }
+)
+
