4ee5bfef77ad75ceffd71d1ee417235d9d19474d
akseong <akseong@uci.edu>
2026-02-24 13:15:46 -0800

    added `deterministic` argument to `get_nn_mod_Ey()`

--- Rcode/gen_preds.R
+++ Rcode/gen_preds.R
@@ -50,37 +50,28 @@ mat0 <- matrix(0, nrow = nrow(curvmat), ncol = sim_params$d_in - length(sim_para
 x_plot <- torch_tensor(cbind(curvmat, mat0))
 
 
-get_nn_mod_Ey <- function(nn_mod, X, ln_fcn = ln_mode){
+get_nn_mod_Ey <- function(nn_mod, X, ln_fcn = ln_mode, deterministic = FALSE){
   # pre_act
   num_layers <- length(nn_mod$children)
   input <- X
   for (nn_layer in 1:num_layers){
-  
-    # log_atilde <- reparameterize(mu = nn_mod$children[[nn_layer]]$atilde_mu, logvar = nn_mod$children[[nn_layer]]$atilde_logvar)
-    # log_btilde <- reparameterize(mu = nn_mod$children[[nn_layer]]$btilde_mu, logvar = nn_mod$children[[nn_layer]]$btilde_logvar)
-    # log_sa <- reparameterize(mu = nn_mod$children[[nn_layer]]$sa_mu, logvar = nn_mod$children[[nn_layer]]$sa_logvar)
-    # log_sb <- reparameterize(mu = nn_mod$children[[nn_layer]]$sb_mu, logvar = nn_mod$children[[nn_layer]]$sb_logvar)
-    # log_s <- 1/2 * (log_sa + log_sb)
-    # log_ztilde <- 1/2 * (log_atilde + log_btilde)
-    # 
-    # log_s$exp() * log_ztilde$exp()
 
-    ztilde <- ln_fcn(mu = as_array(nn_mod$children[[nn_layer]]$atilde_mu + nn_mod$children[[nn_layer]]$btilde_mu)/2,
-            var = exp(as_array(nn_mod$children[[nn_layer]]$atilde_logvar + nn_mod$children[[nn_layer]]$btilde_logvar))/4
+    ztilde <- ln_fcn(
+      mu = as_array(nn_mod$children[[nn_layer]]$atilde_mu + nn_mod$children[[nn_layer]]$btilde_mu)/2,
+      var = exp(as_array(nn_mod$children[[nn_layer]]$atilde_logvar + nn_mod$children[[nn_layer]]$btilde_logvar))/4
     )
     
-    s <- ln_fcn(mu = as_array(nn_mod$children[[nn_layer]]$sa_mu + nn_mod$children[[nn_layer]]$sb_mu)/2,
-            var = exp(as_array(nn_mod$children[[nn_layer]]$sa_logvar + nn_mod$children[[nn_layer]]$sb_logvar))/4
+    s <- ln_fcn(
+      mu = as_array(nn_mod$children[[nn_layer]]$sa_mu + nn_mod$children[[nn_layer]]$sb_mu)/2,
+      var = exp(as_array(nn_mod$children[[nn_layer]]$sa_logvar + nn_mod$children[[nn_layer]]$sb_logvar))/4
     )
-    
-    # z<- (log_s + log_ztilde)$exp()
     
     z <- torch_tensor(ztilde*s)
     
     Xz <- input*z
     
     mu_activations <- nnf_linear(
-      input = Xz, 
+      input = ifelse(deterministic, input, Xz), 
       weight = nn_mod$children[[nn_layer]]$weight_mu, 
       bias = nn_mod$children[[nn_layer]]$bias_mu
     )
@@ -92,7 +83,8 @@ get_nn_mod_Ey <- function(nn_mod, X, ln_fcn = ln_mode){
   }
   
   return(mu_activations)
-}  
+}
+
 
 
 # generate y_news and get sds, quantiles
