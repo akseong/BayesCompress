4c0fe43876d7acdde77014ade42b47812e466a2d
akseong <akseong@uci.edu>
2026-02-25 13:54:51 -0800

    needs more testing

--- Rcode/torch_VC_outputlayer.R
+++ Rcode/torch_VC_outputlayer.R
@@ -1,192 +1,10 @@
 ##################################################
-## Project:   Implementing Horseshoe
-## Date:      Dec 9, 2025
+## Project:   Varying Coefficient model final layer
+## Date:      Feb 25, 2026
 ## Author:    Arnie Seong
 ##################################################
 
-# Kaiming initialization implemented
-
-
-
-library(torch)
-
-reparameterize <- function(mu, logvar, sampling = TRUE, use_cuda) {
-  # for X ~ N(mu, sigma^2), can rewrite as X = mu + sigma * Z
-  # "reparam trick" often used to preserve gradient on mu and sigma
-  # Last modified 2024/07/16
-  # modified 2025/10/27 --- made use_cuda argument extraneous 
-  #   - mucked up computation when moving model off cuda / to cuda 
-  #     because used stored info to decide whether was cuda or not.
-  #   - made extraneous for compatibility with already trained models
-
-  if (sampling) {
-    std <- logvar$mul(0.5)$exp_()
-    if (mu$is_cuda) {
-      eps <- torch_randn(std$size(), device = "cuda", requires_grad = TRUE)
-    } else {
-      eps <- torch_randn(std$size(), device = "cpu", requires_grad = TRUE)
-    }
-    return(mu$add(eps$mul(std)))
-  } else {
-    return(mu)
-  }
-}
-
-
-
-KL_lognorm_gamma <- function(mu, logvar, a = 1/2, b = 1){
-  # mu and logvar must be torch_tensors
-  # computes KL(logNormal(mu, sig^2)  ||  Gamma(a, b))
-  # used for s_a, alpha_i
-  # corrected; full KL:
-  #   - 1/2 * (log(2) + log(pi) + log(sig^2) + 1)
-  #   + log(gamma(a)) + a*(log(b) - mu)
-  #   + 1/b * exp(mu + sig^2 / 2)
-  # want negative KL
-  # expr1 <- -0.5 * (log(2) + log(pi) + logvar + 1)
-  # expr2 <- lgamma(a) - (mu$add(-log(b)))$mul(a)
-  # expr3 <- (mu$add(  (logvar$exp())$mul(1/2)  ))$exp() / b
-  inner1 <- 1 + log(2) + log(pi) + logvar
-  inner2 <- mu + logvar$exp()$mul(1/2)
-  inner1$mul(-0.5) + lgamma(a) + a*log(b) - mu$mul(a) + inner2$exp() / b
-}
-
-
-
-KL_lognorm_IG <- function(mu, logvar, a = 1/2, b = 1){
-  # for s_b, beta_i
-  # i.e. KL(LogNormal q || Inverse-Gamma p)
-  # corrected; full KL:
-  #   - 1/2 * (log(2) + log(pi) + log(sig^2) + 1)
-  #   + log(gamma(a)) + a*(mu - log(b))
-  #   + b * exp(-mu + sig^2 / 2)
-  # same as KL(LogNormal q || Gamma p) if replace mu with -mu, b with 1/b
-  KL_lognorm_gamma(mu = -mu, logvar = logvar, a = a, b = 1/b)
-}
-
-# # r versions used for testing
-# r_negKL_lognorm_gamma <- function(mu, logvar, a = 1/2, b = 1){
-#   # R version - does not work with torch tensors.  Used for testing.
-#   # corrected; full KL:
-#   #   - 1/2 * (log(2) + log(pi) + log(sig^2) + 1)
-#   #   + log(gamma(a)) + a*(log(b) - mu)
-#   #   + 1/b * exp(mu + sig^2 / 2)
-#   # want negative KL
-#   expr1 <- (log(2) + log(pi) + logvar + 1) / 2
-#   expr2 <- lgamma(a) + a*(mu - log(b))
-#   expr3 <- (mu + exp(logvar)/2) / b
-#   
-#   return(expr1 - expr2 - expr3)
-# }
-# r_negKL_lognorm_IG <- function(mu, logvar, a = 1/2, b = 1){
-#   # R version for testing
-#   r_negKL_lognorm_gamma(-mu, logvar, a, 1/b)
-# }
-# 
-# # test conversion to torch, gradients preserved
-# mu <- rnorm(5)
-# logvar <- rnorm(5)
-# a <- 2
-# b <- 2
-# 
-# r_negKL_lognorm_gamma(mu, logvar, a, b)
-# negKL_lognorm_gamma(
-#   mu = torch_tensor(mu, requires_grad = TRUE),
-#   logvar = torch_tensor(logvar, requires_grad = TRUE),
-#   a, b)
-# 
-# r_negKL_lognorm_IG(mu, logvar, a, b)
-# negKL_lognorm_IG(
-#   mu = torch_tensor(mu, requires_grad = TRUE),
-#   logvar = torch_tensor(logvar, requires_grad = TRUE),
-#   a, b)
-
-
-
-
-## fcns to compute expectation and variance of lognormals & fcns of lognormals
-E_lognorm <- function(mu, logvar){
-  # expectation of X ~ logNormal(mu, var)
-  # exp(mu + (sig^2)/2)
-  # ln_mu <- mu$add( logvar$exp()$mul(0.5) )
-  # return(ln_mu$exp())
-  # alternatively:
-  (mu$add(  logvar$exp()$mul(0.5) ))$exp()
-}
-
-
-V_lognorm <- function(mu, logvar){
-  # variance of X ~ logNormal(mu, var)
-  # (exp(var) - 1) * exp(2*mu + var)
-  expr1 <- logvar$exp()$exp() - 1
-  expr2 <- (mu$mul(2))$add(logvar$exp())
-  return(expr1$mul(expr2$exp()))
-}
-
-
-E_sqrt_lognorm <- function(mu, logvar){
-  # if X ~ LN(mu, var), then X = exp(Y) with Y~N(mu, var)
-  # The sqrt(X) = sqrt(exp^Y) = exp^{Y/2} and Y/2 ~ N(mu/2, var/4)
-  E_lognorm(mu/2, logvar - log(4))
-}
-
-V_sqrt_lognorm <- function(mu, logvar){
-  V_lognorm(mu/2, logvar - log(4))
-}
-
-
-
-
-## VARIATIONAL DISTRIBUTION PARAMETERS FOR \tilde{z}, s
-# z_i = \sqrt(atilde_i btilde_i s_a s_b) = ztilde_i s
-## \tilde{z} = \sqrt{\tilde{\alpha}_i \tilde{\beta}_i}    
-##   \sim  LogNormal with 
-##         mu =     \frac{1}{2}   (\mu_{\tilde{alpha}_i} + \mu_{\tilde{beta}_i} )
-##         variance = \frac{1}{4}   (\sigma^2_{\tilde{\alpha}_i} + \sigma^2_{\tilde{\beta}_i}) 
-##
-## NOTE: for \tilde{z} and s, these are not used in training, but in post-hoc analysis
-mu_sqrt_prod_LN <- function(mu_1, mu_2){
-  # \frac{mu_1 + mu_2}{2}
-  (mu_1$add(mu_2))$mul(1/2)
-}
-
-logvar_sqrt_prod_LN <- function(logvar_1, logvar_2){
-  # \frac{sig^2_1 + sig^2_2}{4}
-  ((logvar_1$exp())$add(logvar_2$exp()))$log() - log(4)
-}
-
-log_dropout <- function(hs_layer, type = "local"){
-  # calculates dropout rates based on :
-  # type == "local":    ztilde = sqrt(atilde btilde)
-  # type == "global":    s = sqrt(sa sb)
-  # type == "marginal":    z = ztilde * s
-  
-  if (type == "local"){
-    logvar_sum <- hs_layer$atilde_logvar + hs_layer$btilde_logvar
-  } else if (type == "global"){
-    logvar_sum <- hs_layer$sa_logvar + hs_layer$sb_logvar
-  } else if (type == "marginal"){
-    logvar_sum <- hs_layer$atilde_logvar + hs_layer$btilde_logvar + hs_layer$sa_logvar + hs_layer$sb_logvar
-  }
-  
-  type_var <- exp(logvar_sum - log(4))
-  log_dropout <- log(exp(type_var) - 1)
-  return(log_dropout)
-}
-
-# # fcn testing code
-# library(torch)
-# self <- list()
-# in_features <- 5
-# out_features <- 5
-# use_cuda <- TRUE
-# tau = 1
-# clip_var <- NULL
-# deterministic = FALSE
-# init_weight <- NULL
-# init_bias <- NULL
-
-
+# Last VC model layer ----
 torch_hs_VClast <- nn_module(
   # last modified 10/29/2025 - can now specify initial values for all params
   classname = "VC_horseshoe_final_layer",
@@ -347,7 +165,7 @@ torch_hs_VClast <- nn_module(
     return(alpha)
   },
   
-  forward = function(betas, xvars){
+  forward = function(vcs, xvars){
     # betas generated by penultimate layer
     # want to compute X_i %*% Z %*% betas
     
@@ -366,15 +184,8 @@ torch_hs_VClast <- nn_module(
     # )
     # z <- log_z$exp()
     
-    xz <- xvars*z
-
-    return(
-      nnf_linear(
-        input = xz, 
-        weight = betas, 
-        bias = 0
-      )
-    )
+    xzb <- xvars*z*vcs
+    return(torch_sum(xzb, dim = 2))
   },
   
   
