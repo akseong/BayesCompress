c9965acee92b2e3d29aec78c6efd62400dd41671
Arnold <akseong@uci.edu>
2026-05-16 23:07:15 -0700

    update 5/16

--- final_sims/compile_sims.R
+++ final_sims/compile_sims.R
@@ -93,19 +93,19 @@ metrics_err_by_max_bfdr <- function(dropout_vec, true_vec, bfdr_vec){
 # hshoe4det1_5k_50sims_modfcns_bfdr_arr.Rdata        14 available:         meanfshshoesmallbias_5x16_origmodsupint_p100_mcor.5_5000obs_
 
 
-stem <- here::here("final_sims", "results", "meanfshshoesmallbias_5x16_origmodsupint_p100_mcor.5_5000obs_")
+stem <- here::here("final_sims", "results", "hshoesmallbias_mutcorr0.5_5x165000obs_")
 modfcns_TF <- grepl("meanfs", stem)
 n_sims = 50 
-max_bfdrs <- c(0.01, 0.05, 0.1, 0.25)
+max_bfdrs <- 0:100/100
 
 if (modfcns_TF){
   true_vec <- rep(0, 108)
   true_vec[1:8] <- 1
-  fname_suffix <- "modfcns_bfdr_arr"   
+  fname_suffix <- "modfcns_bfdrall_arr"   
 } else {
   true_vec <- rep(0, 104)
   true_vec[1:4] <- 1
-  fname_suffix <- "origfcns_bfdr_arr"
+  fname_suffix <- "origfcns_bfdrall_arr"
 }
 
 # started off some with 5, some with 10.  Figure out which ones have 5, vs 10
@@ -150,12 +150,14 @@ compiled_fname <- here::here("final_sims", "compiled", paste0(compiled_stem, ".R
 
 
 # store results
-ksn50k_mat <- 
+kglobal50k_mat <- 
+  klocal50k_mat <-
+  ksn50k_mat <- 
   ksntc50k_mat <- 
-  ksn_metrictest_mat <- 
-  ksntc_metrictest_mat <-
-  ktc50k_mat <- 
-  ktc_metrictest_mat <- matrix(NA, nrow = length(sim_fnames), ncol = ncol(sim_res$kappa_mat))
+  ksn_metrictest_mat <-
+  ksntc_metrictest_mat <- 
+  ktc_metrictest_mat <- 
+  ktc50k_mat <- matrix(NA, nrow = length(sim_fnames), ncol = ncol(sim_res$kappa_mat))
 
 perf_mat <- matrix(NA, nrow = n_sims, ncol = 4)
 colnames(perf_mat) <- c("train_sig", "mse_train", "mse_test", "kl")
@@ -171,6 +173,8 @@ for (f_ind in 1:length(sim_fnames)){
   ksn50k_mat[f_ind, ] <- sim_res$kappa_sn_mat[last_epoch,]
   ksntc50k_mat[f_ind, ] <- sim_res$kappa_sntc_mat[last_epoch,]
   ktc50k_mat[f_ind, ] <- sim_res$kappa_tc_mat[last_epoch,]
+  klocal50k_mat[f_ind, ] <- sim_res$kappa_local_mat[last_epoch, ]
+  kglobal50k_mat[f_ind, ] <- sim_res$kappa_mat[last_epoch, ]
   
   row_ind <- get_smallest_testmse_epoch(sim_res$loss_mat)
   perf_mat_metrictest[f_ind, ] <- c(sim_res$sim_params$train_sig, sim_res$loss_mat[row_ind, 2:4])
@@ -187,6 +191,8 @@ for (f_ind in 1:length(sim_fnames)){
 # sum(ksntc50k_mat[, 5:104] < 0.5)  # 1000obs: no FPs            # 5000obs: 100
 
 kmat_list <- list(
+  "kglobal50k_mat" = kglobal50k_mat,
+  "klocal50k_mat" = klocal50k_mat,
   "ktc50k_mat" = ktc50k_mat,
   "ksn50k_mat" = ksn50k_mat,
   "ksntc50k_mat" = ksntc50k_mat,
@@ -197,12 +203,12 @@ kmat_list <- list(
 
 
 # metrictest_rows
-sim_res$loss_mat[metrictest_rows,5] # KLweight at best test_mse
-sum(ksn_metrictest_mat[, 1:4] < 0.05) # 17 out of 300          # 5000obs: 25
-sum(ksn_metrictest_mat[, 5:ncol(sim_res$kappa_mat)] < 0.5)  # no FPs               # 5000obs: 0
-
-sum(ksntc_metrictest_mat[, 1:4] < 0.05) # 156 out of 300       # 5000obs: 313
-sum(ksntc_metrictest_mat[, 5:ncol(sim_res$kappa_mat)] < 0.5)  # 8                  # 5000obs: 11
+# sim_res$loss_mat[metrictest_rows,5] # KLweight at best test_mse
+# sum(ksn_metrictest_mat[, 1:4] < 0.05) # 17 out of 300          # 5000obs: 25
+# sum(ksn_metrictest_mat[, 5:ncol(sim_res$kappa_mat)] < 0.5)  # no FPs               # 5000obs: 0
+# 
+# sum(ksntc_metrictest_mat[, 1:4] < 0.05) # 156 out of 300       # 5000obs: 313
+# sum(ksntc_metrictest_mat[, 5:ncol(sim_res$kappa_mat)] < 0.5)  # 8                  # 5000obs: 11
 
 
 
@@ -230,9 +236,11 @@ errmat_colnames <- c("max_bfdr", "fdr", "bfdr", "FPR", "TPR_sens_recall", "FNR",
 dimnames(ksn50k_bfdr_arr) <- list(paste0("sim_", 1:nrow(ksn50k_mat)), errmat_colnames, paste0("bfdr_",max_bfdrs))
 
 ksntc50k_bfdr_arr <- 
+  ktc50k_bfdr_arr <-
+  klocal50k_bfdr_arr <- 
+  kglobal50k_bfdr_arr <- 
   ksn_metrictest_bfdr_arr <-
   ksntc_metrictest_bfdr_arr <- 
-  ktc50k_bfdr_arr <-
   ktc_metrictest_bfdr_arr <- ksn50k_bfdr_arr
 
 for (k in 1:nrow(ksn50k_mat)){
@@ -252,6 +260,15 @@ for (k in 1:nrow(ksn50k_mat)){
   
   err_mat_tc <- metrics_err_by_max_bfdr(dropout_vec = ktc50k_mat[k, ], true_vec, bfdr_vec = max_bfdrs)
   ktc50k_bfdr_arr[k, , ] <- t(err_mat_tc)
+  
+  err_mat_local <- metrics_err_by_max_bfdr(dropout_vec = klocal50k_mat[k, ], true_vec, bfdr_vec = max_bfdrs)
+  klocal50k_bfdr_arr[k, , ] <- t(err_mat_local)
+  
+  err_mat_global <- metrics_err_by_max_bfdr(dropout_vec = kglobal50k_mat[k, ], true_vec, bfdr_vec = max_bfdrs)
+  kglobal50k_bfdr_arr[k, , ] <- t(err_mat_global)
+  
+  
+  
   
   err_mat_tc_metrictest <- metrics_err_by_max_bfdr(dropout_vec = ktc_metrictest_mat[k, ], true_vec, bfdr_vec = max_bfdrs)
   ktc_metrictest_bfdr_arr[k, , ] <- t(err_mat_tc_metrictest)
@@ -274,6 +291,8 @@ for (k in 1:nrow(ksn50k_mat)){
 bfdr_arr_list <- list(
   "perf_mat" = perf_mat,
   "perf_mat_metrictest" = perf_mat_metrictest,
+  "klocal50k_bfdr_arr" = klocal50k_bfdr_arr,
+  "kglobal50k_bfdr_arr" = kglobal50k_bfdr_arr,
   "ktc50k_bfdr_arr" = ktc50k_bfdr_arr,
   "ksn50k_bfdr_arr" = ksn50k_bfdr_arr,
   "ksntc50k_bfdr_arr" = ksntc50k_bfdr_arr,
@@ -287,142 +306,137 @@ save(res, file = compiled_fname)
 
 cat_color(paste0("results saved to ", compiled_fname))
 
-
-load(compiled_fname)
-
-
-
-t(apply(ktc50k_bfdr_arr, c(2, 3), mean))
-t(apply(ktc50k_bfdr_arr, c(2, 3), sd))
-
-t(apply(ksn50k_bfdr_arr, c(2, 3), mean))
-t(apply(ksn50k_bfdr_arr, c(2, 3), sd))
-
-t(apply(res[[2]]$ksntc50k_bfdr_arr, c(2, 3), mean))
-t(apply(res[[2]]$ksntc50k_bfdr_arr, c(2, 3), sd))
-
-
-t(apply(ktc_metrictest_bfdr_arr, c(2, 3), mean))
-t(apply(ktc_metrictest_bfdr_arr, c(2, 3), sd))
-
-t(apply(ksn_metrictest_bfdr_arr, c(2, 3), mean))
-t(apply(ksn_metrictest_bfdr_arr, c(2, 3), sd))
-
-t(apply(ksntc_metrictest_bfdr_arr, c(2, 3), mean))
-t(apply(ksntc_metrictest_bfdr_arr, c(2, 3), sd))
+# 
+# load(compiled_fname)
+# 
+# 
+# 
+# t(apply(ktc50k_bfdr_arr, c(2, 3), mean))
+# t(apply(ktc50k_bfdr_arr, c(2, 3), sd))
+# 
+# t(apply(ksn50k_bfdr_arr, c(2, 3), mean))
+# t(apply(ksn50k_bfdr_arr, c(2, 3), sd))
+# 
+# t(apply(ksntc50k_bfdr_arr, c(2, 3), mean))
+# t(apply(ksntc50k_bfdr_arr, c(2, 3), sd))
+# 
+# 
+# t(apply(ktc_metrictest_bfdr_arr, c(2, 3), mean))
+# t(apply(ktc_metrictest_bfdr_arr, c(2, 3), sd))
+# 
+# t(apply(ksn_metrictest_bfdr_arr, c(2, 3), mean))
+# t(apply(ksn_metrictest_bfdr_arr, c(2, 3), sd))
+# 
+# t(apply(ksntc_metrictest_bfdr_arr, c(2, 3), mean))
+# t(apply(ksntc_metrictest_bfdr_arr, c(2, 3), sd))
 
 
 
 # #### plot ROC
 # 
-# load(here("final_sims", "compiled", "hshoe2det4_1k_50sims_origfcns_bfdrall_arr.Rdata"))
-# 
-# mat <- res[[2]]$ksntc50k_bfdr_arr
-# #
-# res[[2]]$ksntc50k_bfdr_arr[,,2]
-# 
-# # column 4 is FPR, col 5 is TPR
-# FPRs <- apply(res[[2]]$ksntc50k_bfdr_arr, 3, function(X) X[,4])
-# TPRs <- apply(res[[2]]$ksntc50k_bfdr_arr, 3, function(X) X[,5])
-# fdrs <- apply(res[[2]]$ksntc50k_bfdr_arr, 3, function(X) X[,2])
-# bfdrs <- apply(res[[2]]$ksntc50k_bfdr_arr, 3, function(X) X[,3])
-# 
-# 
-# 
-# 
-# FPR_mean <- apply(FPRs, 2, mean)
-# TPR_mean <- apply(TPRs, 2, mean)
-# FPR_sd <- apply(FPRs, 2, sd)
-# TPR_sd <- apply(TPRs, 2, sd)
-# 
-# fdr_mean <- apply(fdrs, 2, mean)
-# bfdr_mean <- apply(bfdrs, 2, mean)
-# fdr_sd <- apply(fdrs, 2, sd)
-# bfdr_sd <- apply(bfdrs, 2, sd)
-# plot(fdr_mean ~ bfdr_mean)
-# 
-# bfdr_qtiles <-t(apply(bfdrs, 2, function(X) quantile(X, c(0.25, 0.975))))
-# fdr_qtiles <- t(apply(fdrs, 2, function(X) quantile(X, c(0.25, 0.975))))
-# fpr_qtiles <- t(apply(FPRs, 2, function(X) quantile(X, c(0.25, 0.975))))
-# tpr_qtiles <- t(apply(TPRs, 2, function(X) quantile(X, c(0.25, 0.975))))
-# 
-# bfdr_minmax <-t(apply(bfdrs, 2, range))
-# fdr_minmax <- t(apply(fdrs, 2, range))
-# fpr_minmax <- t(apply(FPRs, 2, range))
-# tpr_minmax <- t(apply(TPRs, 2, range))
-# 
-# 
-# 
-# 
+load(here("final_sims", "compiled", "hshoe2det4_1k_50sims_origfcns_bfdrall_arr.Rdata"))
+
+# t(apply(res[[2]]$ksntc50k_bfdr_arr, c(2, 3), mean))
+# t(apply(res[[2]]$ksntc50k_bfdr_arr, c(2, 3), sd))
+
+# column 4 is FPR, col 5 is TPR
+FPRs <-  apply(res[[2]]$ksntc50k_bfdr_arr, 3, function(X) X[,4])
+TPRs <-  apply(res[[2]]$ksntc50k_bfdr_arr, 3, function(X) X[,5])
+fdrs <-  apply(res[[2]]$ksntc50k_bfdr_arr, 3, function(X) X[,2])
+bfdrs <- apply(res[[2]]$ksntc50k_bfdr_arr, 3, function(X) X[,3])
+
+
+
+
+FPR_mean <- apply(FPRs, 2, mean)
+TPR_mean <- apply(TPRs, 2, mean)
+FPR_sd   <- apply(FPRs, 2, sd)
+TPR_sd   <- apply(TPRs, 2, sd)
+
+fdr_mean  <- apply(fdrs, 2, mean)
+bfdr_mean <- apply(bfdrs, 2, mean)
+fdr_sd    <- apply(fdrs, 2, sd)
+bfdr_sd   <- apply(bfdrs, 2, sd)
+plot(fdr_mean ~ bfdr_mean)
+
+bfdr_qtiles <-t(apply(bfdrs, 2, function(X) quantile(X, c(0.25, 0.975))))
+fdr_qtiles  <- t(apply(fdrs, 2, function(X) quantile(X, c(0.25, 0.975))))
+fpr_qtiles  <- t(apply(FPRs, 2, function(X) quantile(X, c(0.25, 0.975))))
+tpr_qtiles  <- t(apply(TPRs, 2, function(X) quantile(X, c(0.25, 0.975))))
+
+bfdr_minmax <-t(apply(bfdrs, 2, range))
+fdr_minmax  <- t(apply(fdrs, 2, range))
+fpr_minmax  <- t(apply(FPRs, 2, range))
+tpr_minmax  <- t(apply(TPRs, 2, range))
+
+
+
+
 # round(t(fdrs[,50:60 ]), 2)
-# max_bfdrs <- 0:100/100
-# roc_df <- data.frame(
-#   "bfdr_threshs" = max_bfdrs,
-#   "FPR_mean" = FPR_mean,
-#   "TPR_mean" = TPR_mean,
-#   "FPR_lo" = fpr_qtiles[,1],
-#   "FPR_hi" = fpr_qtiles[,2],
-#   "TPR_lo" = tpr_qtiles[,1],
-#   "TPR_hi" = tpr_qtiles[,2],
-#   "fdr_mean" = fdr_mean,
-#   "bfdr_mean" = bfdr_mean,
-#   "fdr_lo" = fdr_minmax[, 1],
-#   "fdr_hi" = fdr_minmax[, 2],
-#   "bfdr_lo" = bfdr_minmax[, 1],
-#   "bfdr_hi" = bfdr_minmax[, 2]
-# )
-# 
-# cbind(roc_df$bfdr_threshs, roc_df$fdr_hi)
-# 
-# 
-# library(latex2exp)
-# roc1k <- roc_df %>% 
-#   ggplot() + 
-#   geom_line(aes(y = TPR_mean, x = FPR_mean)) + 
-#   geom_abline(intercept = 0, slope = 1, linetype = "dashed") + 
-#   coord_fixed(ratio = 1) + 
-#   theme(aspect.ratio = 1) +
-#   scale_x_continuous(limits = c(0, 1), expand = c(0, 0)) +
-#   scale_y_continuous(limits = c(0, 1), expand = c(0, 0)) +
-#   labs(subtitle = TeX("ROC curve for HS-2, $n=1000$"),
-#        y = "mean true positive rate",
-#        x = "mean false positive rate")
-# roc1k
-# ggsave(roc1k, filename = here("final_sims", "figs", "roc_hs21k.png"))
+max_bfdrs <- 0:100/100
+roc_df <- data.frame(
+  "bfdr_threshs" = max_bfdrs,
+  "FPR_mean" = FPR_mean,
+  "TPR_mean" = TPR_mean,
+  "FPR_lo" = fpr_qtiles[,1],
+  "FPR_hi" = fpr_qtiles[,2],
+  "TPR_lo" = tpr_qtiles[,1],
+  "TPR_hi" = tpr_qtiles[,2],
+  "fdr_mean" = fdr_mean,
+  "bfdr_mean" = bfdr_mean,
+  "fdr_lo" = fdr_minmax[, 1],
+  "fdr_hi" = fdr_minmax[, 2],
+  "bfdr_lo" = bfdr_minmax[, 1],
+  "bfdr_hi" = bfdr_minmax[, 2]
+)
+
+cbind(roc_df$bfdr_threshs, roc_df$fdr_hi)
+
+
+library(latex2exp)
+roc1k <- roc_df %>%
+  ggplot() +
+  geom_line(aes(y = TPR_mean, x = FPR_mean)) +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
+  coord_fixed(ratio = 1) +
+  theme(aspect.ratio = 1) +
+  scale_x_continuous(limits = c(0, 1), expand = c(0, 0)) +
+  scale_y_continuous(limits = c(0, 1), expand = c(0, 0)) +
+  labs(subtitle = TeX("ROC curve for HS-2, $n=1000$"),
+       y = "mean true positive rate",
+       x = "mean false positive rate")
+roc1k
+ggsave(roc1k, filename = here("final_sims", "figs", "roc_hs2_1k.png"))
 # cbind(TPR_mean, FPR_mean)
-# 
-# bfdr_calibration <- roc_df %>% 
-#   ggplot() + 
-#   geom_line(
-#     aes(
-#       x = bfdr_threshs, y = bfdr_mean
-#     ), color = "blue"
-#   ) + 
-#   geom_ribbon(
-#     aes(
-#       x = bfdr_threshs, ymax = bfdr_hi, ymin = bfdr_lo
-#     ), fill = "blue", alpha = 0.2
-#   ) + 
-#   geom_line(
-#     aes(
-#       x = bfdr_threshs, y = fdr_mean
-#     ), color = "red"
-#   ) + 
-#   geom_ribbon(
-#     aes(
-#       x = bfdr_threshs, ymax = fdr_hi, ymin = fdr_lo
-#     ), fill = "red", alpha = 0.2
-#   ) + 
-#   geom_abline(intercept = 0, slope = 1, linetype = "dashed") + 
-#   labs(
-#     subtitle = "BFDR calibration: BFDR (blue), FDR (red)",
-#     x = "nominal BFDR maximum",
-#     y = "realized BFDR/FDR"
-#   )
-# bfdr_calibration
-# ggsave(bfdr_calibration, filename = here("final_sims", "figs", "bfdr_calib.png"))
-# 
 
 
-
-
+bfdr_calibration <- roc_df %>%
+  ggplot() +
+  geom_line(
+    aes(
+      x = bfdr_threshs, y = bfdr_mean
+    ), color = "blue"
+  ) +
+  geom_ribbon(
+    aes(
+      x = bfdr_threshs, ymax = bfdr_hi, ymin = bfdr_lo
+    ), fill = "blue", alpha = 0.2
+  ) +
+  geom_line(
+    aes(
+      x = bfdr_threshs, y = fdr_mean
+    ), color = "red"
+  ) +
+  geom_ribbon(
+    aes(
+      x = bfdr_threshs, ymax = fdr_hi, ymin = fdr_lo
+    ), fill = "red", alpha = 0.2
+  ) +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
+  labs(
+    subtitle = "BFDR calibration: BFDR (blue), FDR (red)",
+    x = "nominal BFDR maximum",
+    y = "realized BFDR/FDR"
+  )
+bfdr_calibration
+ggsave(bfdr_calibration, filename = here("final_sims", "figs", "bfdr_calib_hs2_1k.png"))
